ee7f7e43c01adf99247ec0e18c03452aef9a42ba
KYapp <kukiyomenaisapp@gmail.com>
2022-08-12 23:56:09 +0900

    コマンドライン引数を追加

--- JToDo/ToDo.cs
+++ JToDo/ToDo.cs
@@ -6,25 +6,54 @@
 
         public static void Main(string[] args)
         {
-            Table[new Title("testtitle")] = new List<Data>()
+            if (args.Length == 0)
             {
-                new Data("testdata"),
-            };
-            Table[new Title("testtitle2")] = new List<Data>()
-            {
-                new Data("testdata2.0"),
-                new Data("testdata2.1")
-            };
-
-            foreach (var title in Table.Keys)
-            {
-                Console.WriteLine(title.title);
-                foreach (var data in Table[title])
+                Table[new Title("testtitle")] = new List<Data>()
                 {
-                    Console.Write("    ");
-                    Console.WriteLine(data.content);
+                    new Data("testdata"),
+                };
+                Table[new Title("testtitle2")] = new List<Data>()
+                {
+                    new Data("testdata2.0"),
+                    new Data("testdata2.1")
+                };
+
+                foreach (var title in Table.Keys)
+                {
+                    Console.WriteLine(title.title);
+                    foreach (var data in Table[title])
+                    {
+                        Console.Write("    ");
+                        Console.WriteLine(data.content);
+                    }
+                    Console.Write('\n');
                 }
-                Console.Write('\n');
+            }
+            else
+            {
+                switch (args[0])
+                {
+                    case "help":
+                        Console.WriteLine("まだヘルプは作ってないです。");
+                        break;
+                    case "version":
+                        Console.WriteLine("0.0.0");
+                        break;
+                    case "open":
+                        if (args.Length == 2)
+                        {
+                            string path = args[1];
+
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("指定されたパスは存在しないか、開くことができるデータ形式ではありません。");
+                        }
+                        break;
+                    default:
+                        break;
+                }
             }
         }
     }
